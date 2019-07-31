626dc5af815519c40679f9e37c5c6c115b01edd3
Jan Skowronski <crianonim@gmail.com>
2019-07-31 16:37:58 +0100

    Testing content of / vs index.html

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,6 +1,8 @@
 const router = require("../src/router");
 const test = require("tape");
 const supertest = require("supertest");
+const fs = require('fs');
+const path = require('path');
 
 test("Not found route test", t => {
   supertest(router)
@@ -21,3 +23,16 @@ test("Error on server side", t => {
       t.end();
     });
 });
+
+test("Is the / route link to /public/html", t =>{
+    supertest(router)
+     .get('/')
+     .expect(200)
+     .expect("content-type",/html/)
+     .end( (err,res)=>{
+         fs.readFile(path.join(__dirname,'..','/public/index.html'),'utf8',(err,content)=>{
+             t.equal(res.text,content,"Should have the same content");
+             t.end();
+         })
+     })
+})
