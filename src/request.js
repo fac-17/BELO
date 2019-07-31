e0dfe04a5299096ed53a2f241012d80b795e910c
Gregor <gregorlthomson@sky.com>
2019-07-31 15:10:26 +0100

    Add Skeleton Logic

--- src/request.js
+++ src/request.js
@@ -1,2 +1,30 @@
 const http = require("http");
-const myRequest = (url, callback = (err, res) => {}) => {};
+const myUrl = "www.google.com";
+
+const myObj = {
+  statusCode: 200,
+  body: {
+    hisname: "Emmanuel",
+    myname: "Gregor"
+  }
+};
+
+const callback = (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res.body.myname);
+    // Handle response
+  }
+};
+
+const myRequest = (url, cb) => {
+  if (!url) {
+    cb("No URL was found");
+  } else {
+    cb(null, myObj);
+  }
+};
+
+myRequest(myUrl, callback);
+myRequest(null, callback);
