eb105c1eb549dc01d5bc6b89520c8ee5da69ba52
Jan Skowronski <crianonim@gmail.com>
2019-08-01 11:19:09 +0100

    Test actual api

--- src/logic.js
+++ src/logic.js
@@ -1,6 +1,7 @@
 const env2=require('env2')('./api.env');
 const API_KEY=process.env.API_KEY;
-const prepareAPIcallURL = (searchTerm, apiKEY) => {
+const URL="https://newsapi.org/v2/everything"
+const prepareAPIcallURL = (searchTerm) => {
     return "love island";
 };
 
--- tests/logic.test.js
+++ tests/logic.test.js
@@ -2,15 +2,22 @@ const test = require("tape");
 const logic = require("../src/logic");
 
 test("prepareAPIcallURL returns a string", t => {
-  const actual = typeof logic.prepareAPIcallURL("love island", "666");
+  const actual = typeof logic.prepareAPIcallURL("love island");
   t.equal(actual, "string");
   t.end();
 });
 
 test("prepareAPIcallURL returns a string that contains search term", t => {
   const actual = logic
-    .prepareAPIcallURL("love island", "666")
+    .prepareAPIcallURL("love island")
     .includes("love island");
   t.equal(actual, true);
   t.end();
 });
+
+test("prepareAPIcallURL returns an actual URL for a single word search term", t=>{
+    const actual = logic.prepareAPIcallURL("love");
+    const expected = "https://newsapi.org/v2/everything?q=love";
+    t.equal(actual,expected);
+    t.end();
+})
