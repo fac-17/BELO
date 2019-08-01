c7795091fa3f75954a14ad9d6f311997fc1e656f
Jan Skowronski <crianonim@gmail.com>
2019-08-01 11:29:29 +0100

    change .env file

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,4 @@
-const env2=require('env2')('./api.env');
+const env2=require('env2')('./.env');
 const API_KEY=process.env.API_KEY;
 const URL="https://newsapi.org/v2/everything"
 const prepareAPIcallURL = (searchTerm) => {
