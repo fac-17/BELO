a0fca46f09323bf2a1a688e75957acca3500a47d
tonylomax <antl.lomax@gmail.com>
2019-08-01 10:56:10 +0100

    Front end call to back end

--- public/index.js
+++ public/index.js
@@ -1,1 +1,19 @@
 console.log("INIT");
+
+let xml = new XMLHttpRequest();
+let APIResponse;
+
+const backendCall = queryInput => {
+  let url = `/everything?q=${queryInput}`;
+  xml.onreadystatechange = () => {
+    if (xml.readyState === 4 && xml.status === 200) {
+      APIResponse = JSON.parse(xml.responseText);
+    } else {
+      console.error(xml.responseText);
+    }
+  };
+  xml.open("GET", url);
+  xml.send();
+};
+
+console.log(APIResponse);
