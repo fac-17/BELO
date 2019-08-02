e98fbe7754177fa53f77d300c3654b7e75b7c113
Jan Skowronski <crianonim@gmail.com>
2019-08-02 11:29:43 +0100

    Works with both http and https

--- src/request.js
+++ src/request.js
@@ -1,10 +1,12 @@
 const https = require("https");
+const http = require('http');
 
 const myRequest = (url, cb) => {
   if (!url) {
     cb("No URL was found");
   } else {
-    https.get(url, res => {
+    let protocolHandler = url.startsWith('https')? https : http;
+    protocolHandler.get(url, res => {
       let data = "";
       res.on("data", chunk => {
         data += chunk;
