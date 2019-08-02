fc1ffb82a0a22e6c526dd1a93401735879c660f7
Gregor <gregorlthomson@sky.com>
2019-08-02 11:59:05 +0100

    Add and test Nock test

--- tests/request.test.js
+++ tests/request.test.js
@@ -1,5 +1,6 @@
 const test = require("tape");
 const request = require("../src/request");
+const nock = require("nock");
 
 test("Tape is working", t => {
   t.pass("Tape is working");
@@ -26,19 +27,17 @@ test("myRequest correctly fetches data from https", t => {
   });
 });
 
-// test("myRequest correcly fetches data", t => {
-//   nock("https://jsonplaceholder.typicode.com")
-//     .get("/todos/1")
-//     .reply(200, { title: "delectus aut autem" });
-//   request.myRequest("https://jsonplaceholder.typicode.com/todos/1", (err, res) => {
-//       t.error(error);
-//       t.equal(
-//           res.statusCode, 200,
-//           "the API should respoinse with a status code of 200"
-//       ),
-//       t.deepEqual(
-//           res.statusCode
-//       )
-//       )
-//   });
-// });
+test("myRequest correcly fetches data", t => {
+  nock("https://jsonplaceholder.typicode.com")
+    .get("/todos/1")
+    .reply(200, { title: "delectus aut autem" });
+  request("https://jsonplaceholder.typicode.com/todos/1", (err, res) => {
+    t.error(err);
+    t.equal(
+      res.statusCode,
+      200,
+      "the API should respoinse with a status code of 200"
+    );
+    t.end();
+  });
+});
